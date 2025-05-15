944550471291b1e067233f50296985f325bf6138
aleneresss <alessandroneres166@gmail.com>
2025-05-15 12:05:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const tabelasConfig = {
     "PARANÁ TAC": { taxa: 1.79, calcTac: (v) => aplicarAlíquotaPtac(v), calcMeta: (t) => t * 0.6956 }, 
     "PARANÁ": { taxa: 1.79, calcTac: (v) => v, calcMeta: (t) => t * 0.6956 },
-    "PARANÁ SEG": { taxa: 1.79, calcTac: (v, e) => Math.max(v - (e/(100/22)),v-600), calcMeta: (t) => t * 0.6956 },
+    "PARANÁ SEG": { taxa: 1.79, calcTac: (v, e) => Math.max(v-(e / (100/6)), v - 600), calcMeta: (t) => t * 0.6956 },
     "SENNA": { taxa: 1.8, calcTac: (v, e) => v - (e/(100/22)), calcMeta: (t) => t * 1.10 },
     "PRIME": { taxa: 1.8, calcTac: (v) => v - 70, calcMeta: (t) => t * 0.68 },
     "MONACO": { taxa: 1.8, calcTac: (v, e) => v * 0.815, calcMeta: (t) => t * 0.90 },
